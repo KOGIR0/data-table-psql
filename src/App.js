1f6d8ded197ab3b7dfe2a12693d6eb1f50563776
Igor Kondrahov <kondrahov.2000@mail.ru>
2022-01-10 23:23:13 +0300

    Added sorting to table

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ class App extends React.Component {
     super(props);
     this.state = {
       data: [],
+      direction: {
+        name: "asc",
+        ammount: "asc",
+        distance: "asc",
+      },
+      searchColumn: "",
+      searchType: "",
+      searchValue: "",
     };
   }
 
@@ -23,28 +31,76 @@ class App extends React.Component {
       });
   }
 
+  sortByKey(key) {
+    let direction = this.state.direction;
+    const asc = direction[key] === "asc";
+    let sortedData = this.state.data.sort((a, b) => {
+      if (a[key] < b[key]) {
+        return asc ? -1 : 1;
+      } else if (a[key] > b[key]) {
+        return asc ? 1 : -1;
+      } else {
+        return 0;
+      }
+    });
+    direction[key] = asc ? "desc" : "asc";
+
+    this.setState({
+      data: sortedData,
+      direction: direction,
+    });
+  }
+
   render() {
     return (
-      <table className="App">
-        <thead>
-          <th>Date</th>
-          <th>Name</th>
-          <th>Ammount</th>
-          <th>Distance</th>
-        </thead>
-        <tbody>
-          {this.state.data.map((element) => {
-            return (
-              <tr>
-                <td>{element.date}</td>
-                <td>{element.name}</td>
-                <td>{element.ammount}</td>
-                <td>{element.distance}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div>
+        <div>
+          <select
+            name="columns"
+            id="search-column"
+            onChange={(e) => this.setState({ searchColumn: e.target.value })}
+          >
+            <option value="date">Date</option>
+            <option value="name">Name</option>
+            <option value="ammount">Ammount</option>
+            <option value="distance">Distance</option>
+          </select>
+          <select
+            name="type"
+            id="search-type"
+            onChange={(e) => this.setState({ searchType: e.target.value })}
+          >
+            <option value="equal">Equal</option>
+            <option value="contains">Contains</option>
+            <option value="bigger">Bigger</option>
+            <option value="smaller">Smaller</option>
+          </select>
+          <input
+            id="search-value"
+            onChange={(e) => this.setState({ searchValue: e.target.value })}
+          ></input>
+        </div>
+        <table className="App">
+          <thead>
+            <th>Date</th>
+            <th onClick={() => this.sortByKey("name")}>Name</th>
+            <th onClick={() => this.sortByKey("ammount")}>Ammount</th>
+            <th onClick={() => this.sortByKey("distance")}>Distance</th>
+          </thead>
+          <tbody>
+            {this.state.data.map((element) => {
+              return (
+                <tr>
+                  <td>{element.date}</td>
+                  <td>{element.name}</td>
+                  <td>{element.ammount}</td>
+                  <td>{element.distance}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
