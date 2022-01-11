84b521cc8fcf34a762d369a4be84ce39f2792fb6
Igor Kondrahov <kondrahov.2000@mail.ru>
2022-01-11 23:18:33 +0300

    Changed options

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 function Select(props) {
+  const options = props.data.map((value) => (
+    <option value={value.toLowerCase()}>{value}</option>
+  ));
+
   return (
     <select name={props.name} onChange={props.onSelect}>
-      {props.data.map((value) => {
-        return <option value={value.toLowerCase()}>{value}</option>;
-      })}
+      {options}
     </select>
   );
 }
